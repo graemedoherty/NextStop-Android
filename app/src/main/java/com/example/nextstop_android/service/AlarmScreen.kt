399c9b0877bf80dd505be588272e36312435522f
Graeme Doherty <graeme.doherty@gmail.com>
2026-01-21 13:27:08 +0000

    alarm fixed and json minified

--- app/src/main/java/com/example/nextstop_android/service/AlarmScreen.kt
+++ app/src/main/java/com/example/nextstop_android/service/AlarmScreen.kt
@@ -2,11 +2,28 @@ package com.example.nextstop_android.service
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronRight
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,11 +32,9 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.nextstop_android.R // 🔑 Make sure to import your R file
-import com.example.nextstop_android.ui.theme.NextStopAndroidTheme
+import com.example.nextstop_android.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -61,7 +76,7 @@ fun AlarmScreen(
             Spacer(modifier = Modifier.height(48.dp))
 
             Text(
-                text = "ARRIVED AT",
+                text = "Next stop",
                 style = MaterialTheme.typography.labelMedium,
                 color = Color.Gray,
                 letterSpacing = 2.sp
